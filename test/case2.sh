8973fee6fca5dff898fdf3c9ec88550e59994922
Jacco Flenter <jacco@wercker.com>
2013-10-03 18:09:32 +0200

    ignore branches support/test added.
    Note: a build with the same commit hash will always have the same build number

---
+++ test/case2.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/env bash
+
+echo "Case 1: server not running. Script should return an error."
+
+echo ""
+echo "Initial environment variables: "
+export
+echo ""
+source $WERCKER_STEP_ROOT/run.sh
+
+RESULT=$?
+
+if [[ $RESULT = "0" ]]; then
+
+	if [[ $GENERATED_BUILD_NR = "1" ]]; then
+		echo ""
+		echo "! changing commit hash !"
+		TMP_WERCKER_GIT_COMMIT=$WERCKER_GIT_COMMIT
+		WERCKER_GIT_COMMIT+="_new_commit"
+
+		echo "new commit hash: $WERCKER_GIT_COMMIT"
+		echo ""
+
+		source $WERCKER_STEP_ROOT/run.sh
+
+		RESULT=$?
+
+		if [[ $RESULT = "0" ]]; then
+
+			if [[ $GENERATED_BUILD_NR = "2" ]]; then
+
+				echo ""
+				echo "! changing branch !"
+				TMP_WERCKER_GIT_BRANCH=$WERCKER_GIT_BRANCH
+				WERCKER_GIT_BRANCH="feature/this"
+
+				echo "new branch $WERCKER_GIT_BRANCH"
+				echo ""
+
+				source $WERCKER_STEP_ROOT/run.sh
+
+				RESULT=$?
+
+				if [[ $RESULT = "0" ]]; then
+
+					if [[ $GENERATED_BUILD_NR = "2" ]]; then
+
+						echo ""
+						echo "! Querying original branch/commit hash again. !"
+						WERCKER_GIT_COMMIT=$TMP_WERCKER_GIT_COMMIT
+						WERCKER_GIT_BRANCH=$TMP_WERCKER_GIT_BRANCH
+
+						source $WERCKER_STEP_ROOT/run.sh
+
+						RESULT=$?
+
+						if [[ $RESULT = "0" ]]; then
+
+							if [[ $GENERATED_BUILD_NR = "1" ]]; then
+
+								echo ""
+								echo "Test: OK"
+							else
+							    echo "Test: FAIL"
+							    return 1 2>/dev/null || exit 1
+							fi
+
+						else
+						    echo "Test: FAIL"
+						    return 1 2>/dev/null || exit 1
+						fi
+					else
+					    echo "Test: FAIL"
+					    return 1 2>/dev/null || exit 1
+					fi
+
+				else
+				    echo "Test: FAIL"
+				    return 1 2>/dev/null || exit 1
+				fi
+			else
+			    echo "Test: FAIL"
+			    return 1 2>/dev/null || exit 1
+			fi
+
+		else
+		    echo "Test: FAIL"
+		    return 1 2>/dev/null || exit 1
+		fi
+
+	else
+	    echo "Test: FAIL"
+	    return 1 2>/dev/null || exit 1
+	fi
+
+else
+    echo "Test: FAIL"
+    return 1 2>/dev/null || exit 1
+fi
+
