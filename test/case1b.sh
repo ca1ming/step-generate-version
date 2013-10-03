69b6067e7e1ec61f44dedd1079ce6d8cb82eddb3
Jacco Flenter <jacco@wercker.com>
2013-10-03 17:16:59 +0200

    case 1b added

---
+++ test/case1b.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "Case 1: server not running. Script should return an error."
+
+echo ""
+echo "Initial environment variables: "
+export
+echo ""
+source $WERCKER_STEP_ROOT/run.sh
+
+RESULT=$?
+echo "Results: $RESULT"
+
+if [[ $RESULT = "0" ]]; then
+    echo "Test: OK"
+else
+    echo "Test: FAIL"
+    return 1 2>/dev/null || exit 1
+fi
